1e68cb963e339c9b1f612757f04859a855e58cf8
bigdragon <ryudai-anada@keio.jp>
2021-11-27 11:29:38 +0900

    init.sql

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -5,23 +5,26 @@ USE webapp;
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  language VARCHAR(255) NOT NULL
+  language VARCHAR(255) NOT NULL,
+  color VARCHAR(255) NOT NULL
 );
 
-INSERT INTO languages (language) VALUES ("JavaScript"), ("CSS"), ("PHP"), ("HTML"), ("Laravel"), ("SQL"), ("SHELL"), ("情報システム基礎知識(その他)");
+INSERT INTO languages (language, color) VALUES ("JavaScript", "0346EC"), ("CSS", "1071BD"), ("PHP", "20BEDE"), ("HTML", "3CCEFE"), ("Laravel", "B29EF3"), ("SQL", "6D46EC"), ("SHELL", "4A18EF"), ("情報システム基礎知識(その他)", "3105C0");
 
 DROP TABLE IF EXISTS contents;
 CREATE TABLE contents (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  content VARCHAR(255) NOT NULL
+  content VARCHAR(255) NOT NULL,
+  color VARCHAR(255) NOT NULL
 );
 
-INSERT INTO contents (content) VALUES ("ドットインストール"), ("N予備校"), ("POSSE課題");
+INSERT INTO contents (content, color) VALUES ("ドットインストール", "0346EC"), ("N予備校", "1071BD"), ("POSSE課題", "20BEDE");
 
 DROP TABLE IF EXISTS records;
 CREATE TABLE records (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   record_date DATE,
   language_id TINYINT,
-  content_id TINYINT
+  content_id TINYINT,
+  studytime TINYINT
 );
