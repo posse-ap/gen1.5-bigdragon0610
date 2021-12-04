b56d72b615cb7387d9d3af2c217e4429353104db
bigdragon <ryudai-anada@keio.jp>
2021-12-04 11:01:32 +0900

    キャッシュは消そうね

--- posse-3rd-laravel-study/app/Http/Controllers/HelloController.php
+++ posse-3rd-laravel-study/app/Http/Controllers/HelloController.php
@@ -2,28 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Console\Presets\React;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index() {
-
-        return <<<EOF
-        <html>
-        <head>
-        <title>Hello/Index</title>
-        <style>
-        body { font-size:16pt; color:#999; }
-        h1 { font-size:100pt; text-align:right; color:#eee;
-            margin: -40px 0px -50px 0px; }
-        </style>
-        </head>
-        <body>
-            <h1>Index</h1>
-            <p>これは、Ｈelloコントローラのindexアクションです。</p>
-        </body>
-        </html>
-        EOF;
-
-    }
+  public function index()
+  {
+    return view('hello.index', ['message'=>'Hello!']);
+  }
 }
