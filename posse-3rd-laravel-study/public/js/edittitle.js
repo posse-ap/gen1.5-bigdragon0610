ae222157b6b6bd65308568a96681acd10d0300fd
bigdragon <ryudai-anada@keio.jp>
2022-01-26 21:54:44 +0900

    .gitignore反映させる

--- posse-3rd-laravel-study/public/js/edittitle.js
+++ posse-3rd-laravel-study/public/js/edittitle.js
@@ -8,8 +8,8 @@ for (i = title_count - 1; i >= 0; i--) {
 }
 
 // 編集ボタンのクリック処理
-title_button.forEach((button, i) => {
-  button.addEventListener('click', () => {
+title_button.forEach((value, i) => {
+  value.addEventListener('click', () => {
     let edit_title = document.getElementById(`edit_title_${i}`);
     let link_title = document.getElementById(`link_title_${i}`);
     let edit_title_button = document.getElementById(`edit_title_button_${i}`);
@@ -20,55 +20,3 @@ title_button.forEach((button, i) => {
     edit_submit_button.classList.remove('invisible');
   });
 });
-
-// sort用のinput、↑ボタン、↓ボタンを取得
-const input_sort = [];
-const sort_up = [];
-const sort_down = [];
-for (i = title_count - 1; i >= 0; i--) {
-  input_sort[i] = document.getElementById(`input_sort_${i}`);
-  sort_up[i] = document.getElementById(`sort_up_${i}`);
-  sort_down[i] = document.getElementById(`sort_down_${i}`);
-}
-
-const title_table_main = document.getElementById('title_table_main');
-
-sort_up.forEach((button, i) => {
-  button.addEventListener('click', () => {
-    let clicked_sort = parseInt(input_sort[i].value);
-    if (clicked_sort == 0) {
-      ;
-    } else {
-      let above_clicked_sort = document.querySelector(`[data-sort="${clicked_sort - 1}"]`);
-      above_clicked_sort.value = clicked_sort;
-      above_clicked_sort.dataset.sort = `${clicked_sort}`;
-      input_sort[i].value = clicked_sort - 1;
-      input_sort[i].dataset.sort = `${clicked_sort - 1}`;
-      let clicked_title = document.querySelector(`[data-title_index="${clicked_sort}"]`);
-      let above_clicked_title = document.querySelector(`[data-title_index="${clicked_sort - 1}"]`);
-      title_table_main.insertBefore(clicked_title, above_clicked_title);
-      clicked_title.dataset.title_index = `${clicked_sort - 1}`;
-      above_clicked_title.dataset.title_index = `${clicked_sort}`;
-    }
-  })
-})
-
-sort_down.forEach((button, i) => {
-  button.addEventListener('click', () => {
-    let clicked_sort = parseInt(input_sort[i].value);
-    if (clicked_sort == title_count -1) {
-      ;
-    } else {
-      let below_clicked_sort = document.querySelector(`[data-sort="${clicked_sort + 1}"]`);
-      below_clicked_sort.value = clicked_sort;
-      below_clicked_sort.dataset.sort = `${clicked_sort}`;
-      input_sort[i].value = clicked_sort + 1;
-      input_sort[i].dataset.sort = `${clicked_sort + 1}`;
-      let clicked_title = document.querySelector(`[data-title_index="${clicked_sort}"]`);
-      let below_clicked_title = document.querySelector(`[data-title_index="${clicked_sort + 1}"]`);
-      title_table_main.insertBefore(below_clicked_title, clicked_title);
-      clicked_title.dataset.title_index = `${clicked_sort + 1}`;
-      below_clicked_title.dataset.title_index = `${clicked_sort}`;
-    }
-  })
-})
--- posse-3rd-laravel-study/resources/views/edittitle/index.blade.php
+++ posse-3rd-laravel-study/resources/views/edittitle/index.blade.php
@@ -19,50 +19,35 @@
 
   <section class="all_titles">
     <h2>タイトル一覧</h2>
-    <div id="title_table">
-      <div>
-        <div>タイトル</div>
-        <div>編集</div>
-        <div>削除</div>
-      </div>
-      <div>
-        <div id="title_table_main">
-          @foreach($areas as $area)
-          <div data-title_index="{{$loop->index}}">
+    <table border="1">
+      <tr>
+        <th>タイトル</th>
+        <th>編集</th>
+        <th>削除</th>
+      </tr>
+        @foreach($areas as $area)
+          <tr data-title_index="{{$loop->index}}">
             <form action="{{route('edittitle.update', $area->id)}}" method="POST">
               @csrf
               @method('PUT')
-              <div>
+              <td>
                 <a href="quizy/1" id="link_title_{{$loop->index}}">{{$area->name}}の難読地名クイズ</a>
                 <div id="edit_title_{{$loop->index}}" class="invisible"><input type="text" name="name" value="{{$area->name}}">の難読地名クイズ</div>
-              </div>
-              <div>
+              </td>
+              <td>
                 <div id="edit_title_button_{{$loop->index}}" class="edit_title_button">編集</div>
                 <input type="submit" value="OK" id="edit_submit_button_{{$loop->index}}" class="invisible">
-              </div>
+              </td>
             </form>
-            <div>
+            <td>
               <form action="{{route('edittitle.destroy', $area->id)}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <input type="submit" value="削除">
               </form>
-            </div>
-          </div>
-          @endforeach
-        </div>
-        <form action="{{route('edittitle.update_sort', $area->id)}}" method="POST">
-          @csrf
-          @method('PUT')
-          @for($i = 0; $i < count($areas); $i++)
-            <div>
-              <input id="input_sort_{{$i}}" data-sort="{{$i}}" type="hidden" value="{{$i}}">
-              <div id="sort_up_{{$i}}" style="cursor: pointer;">↑</div>
-              <div id="sort_down_{{$i}}" style="cursor: pointer;">↓</div>
-            </div>
-          @endfor
-        </form>
-      </div>
-    </div>
+            </td>
+          </tr>
+        @endforeach
+    </table>
   </section>
 @endsection
