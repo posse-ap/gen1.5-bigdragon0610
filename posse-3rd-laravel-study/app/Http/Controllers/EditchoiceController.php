4c027e6f55c1c6a728a231fbedeed5600b38abc8
ryudai <bigdragon.06102001@gmail.com>
2022-03-12 15:05:21 +0900

    redirect処理をきれいにした。シュートくんがback()見つけてくれた。

--- posse-3rd-laravel-study/app/Http/Controllers/EditchoiceController.php
+++ posse-3rd-laravel-study/app/Http/Controllers/EditchoiceController.php
@@ -33,21 +33,15 @@ class EditchoiceController extends Controller
   /**
    * Store a newly created resource in storage.
    * 
-   * @param  int  $area_id
-   * @param  int  $question_id
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
-  public function store(Request $request, $area_id, $question_id)
+  public function store(Request $request)
   {
     $form = $request->all();
     unset($form['_token']);
     Choice::create($form);
-    return redirect()
-      ->route('edittitle.editquestion.editchoice.index', [
-        'edittitle' => $area_id,
-        'editquestion' => $question_id,
-      ])
+    return back()
       ->with('success', '登録完了しました');
   }
 
@@ -78,41 +72,29 @@ class EditchoiceController extends Controller
    *
    * @param  \Illuminate\Http\Request  $request
    * 
-   * @param  int  $area_id
-   * @param  int  $question_id
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $area_id, $question_id, $id)
+  public function update(Request $request, $id)
   {
     $update = [
       'choice' => $request->choice
     ];
     Choice::where('id', $id)->update($update);
-    return redirect()
-      ->route('edittitle.editquestion.editchoice.index', [
-        'edittitle' => $area_id,
-        'editquestion' => $question_id,
-      ])
+    return back()
       ->with('success', '変更完了しました');
   }
 
   /**
    * Remove the specified resource from storage.
    *
-   * @param  int  $area_id
-   * @param  int  $question_id
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function destroy($area_id, $question_id, $id)
+  public function destroy($id)
   {
     Choice::where('id', $id)->delete();
-    return redirect()
-      ->route('edittitle.editquestion.editchoice.index', [
-        'edittitle' => $area_id,
-        'editquestion' => $question_id,
-      ])
+    return back()
       ->with('success', '削除完了しました');
   }
 }
