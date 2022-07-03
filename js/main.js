ed40f682531859af023d596b353118804dffbc70
ryudai <bigdragon.06102001@gmail.com>
2022-07-04 01:07:06 +0900

    ballをランダム数個出現させる

--- js/main.js
+++ js/main.js
@@ -6,19 +6,18 @@
   }
 
   class Ball {
-    constructor(canvas) {
-      this.canvas = canvas;
+    constructor(balls) {
+      this.balls = balls;
+      this.canvas = this.balls.canvas;
+      this.paddle = this.balls.paddle;
+      this.game = this.balls.game;
       this.ctx = this.canvas.getContext("2d");
       this.x = rand(30, 250);
       this.y = 30;
       this.r = 10;
       this.vx = rand(2, 4) * (Math.random() < 0.5 ? 1 : -1);
       this.vy = rand(2, 4);
-      this.isMissed = false;
-    }
-
-    getMissedStatus() {
-      return this.isMissed;
+      this.visible = true;
     }
 
     bounce() {
@@ -27,6 +26,10 @@
 
     reposition(paddleTop) {
       this.y = paddleTop - this.r;
+    }
+
+    hide() {
+      this.visible = false;
     }
 
     getX() {
@@ -42,11 +45,30 @@
     }
 
     update() {
+      const ballBottom = this.getY() + this.getR();
+      const paddleTop = paddle.y;
+      const ballTop = this.getY() - this.getR();
+      const paddleBottom = paddle.y + paddle.h;
+      const ballCenter = this.getX();
+      const paddleLeft = paddle.x;
+      const paddleRight = paddle.x + paddle.w;
+      if (
+        ballBottom > paddleTop &&
+        ballTop < paddleBottom &&
+        ballCenter > paddleLeft &&
+        ballCenter < paddleRight
+      ) {
+        this.bounce();
+        this.reposition(paddleTop);
+        this.game.addScore();
+      }
+
       this.x += this.vx;
       this.y += this.vy;
 
-      if (this.y - this.r > this.canvas.height) {
-        this.isMissed = true;
+      if (this.y - this.r > this.canvas.height && this.visible) {
+        this.balls.reduceBallNum();
+        this.hide();
       }
 
       if (this.x - this.r < 0 || this.x + this.r > this.canvas.width) {
@@ -67,9 +89,8 @@
   }
 
   class Paddle {
-    constructor(canvas, game) {
+    constructor(canvas) {
       this.canvas = canvas;
-      this.game = game;
       this.ctx = this.canvas.getContext("2d");
       this.w = 60;
       this.h = 16;
@@ -85,32 +106,14 @@
       });
     }
 
-    update(ball) {
-      const ballBottom = ball.getY() + ball.getR();
-      const paddleTop = this.y;
-      const ballTop = ball.getY() - ball.getR();
-      const paddleBottom = this.y + this.h;
-      const ballCenter = ball.getX();
-      const paddleLeft = this.x;
-      const paddleRight = this.x + this.w;
-
-      if (
-        ballBottom > paddleTop &&
-        ballTop < paddleBottom &&
-        ballCenter > paddleLeft &&
-        ballCenter < paddleRight
-      ) {
-        ball.bounce();
-        ball.reposition(paddleTop);
-        this.game.addScore();
-      }
-
+    update() {
       const rect = this.canvas.getBoundingClientRect();
       this.x = this.mouseX - rect.left - this.w / 2;
 
       if (this.x < 0) {
         this.x = 0;
       }
+
       if (this.x + this.w > this.canvas.width) {
         this.x = this.canvas.width - this.w;
       }
@@ -122,23 +125,85 @@
     }
   }
 
-  class Game {
-    constructor(canvas) {
+  class Balls {
+    constructor(ballNum, canvas, paddle, game) {
+      this.ballNum = ballNum;
       this.canvas = canvas;
-      this.ctx = this.canvas.getContext("2d");
-      this.ball = new Ball(this.canvas);
-      this.paddle = new Paddle(this.canvas, this);
-      this.loop();
-      this.isGameOver = false;
-      this.score = 0;
+      this.paddle = paddle;
+      this.game = game;
+      this.elements = [];
+      for (let i = 1; i <= ballNum; i++) {
+        this.elements.push(new Ball(this));
+      }
     }
 
-    addScore() {
-      this.score++;
+    addElement(element) {
+      this.elements.push(element);
+    }
+
+    reduceBallNum() {
+      this.ballNum--;
+    }
+
+    update() {
+      if (this.ballNum === 0) {
+        this.game.gameOver();
+      }
+
+      this.elements.forEach((element) => {
+        element.update();
+      });
+    }
+
+    draw() {
+      this.elements.forEach((element) => {
+        element.draw();
+      });
+    }
+  }
+
+  class Screen {
+    constructor(canvas, paddle, balls, game) {
+      this.canvas = canvas;
+      this.paddle = paddle;
+      this.balls = balls;
+      this.game = game;
+      this.ctx = this.canvas.getContext("2d");
+      this.loop();
+    }
+
+    update() {
+      this.paddle.update();
+      this.balls.update();
+    }
+
+    draw() {
+      if (this.game.isGameOver) {
+        this.drawGameOver();
+        return;
+      }
+
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.paddle.draw();
+      this.balls.draw();
+      this.drawScore();
+    }
+
+    drawScore() {
+      this.ctx.font = "20px Arial";
+      this.ctx.fillStyle = "#fdfdfd";
+      this.ctx.fillText(this.game.score, 10, 25);
+    }
+
+    drawGameOver() {
+      this.ctx.font = '28px "Arial Black"';
+      this.ctx.fillStyle = "tomato";
+      this.ctx.fillText("Game Over", 50, 150);
     }
 
     loop() {
-      if (this.isGameOver) {
+      if (this.game.isGameOver) {
+        this.drawGameOver();
         return;
       }
 
@@ -149,44 +214,31 @@
         this.loop();
       });
     }
+  }
 
-    update() {
-      this.ball.update();
-      this.paddle.update(this.ball);
-
-      if (this.ball.getMissedStatus()) {
-        this.isGameOver = true;
-      }
+  class Game {
+    constructor() {
+      this.isGameOver = false;
+      this.score = 0;
     }
 
-    draw() {
-      if (this.isGameOver) {
-        this.drawGameOver();
-        return;
-      }
-
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.ball.draw();
-      this.paddle.draw();
-      this.drawScore();
+    addScore() {
+      this.score++;
     }
 
-    drawGameOver() {
-      this.ctx.font = '28px "Arial Black"';
-      this.ctx.fillStyle = "tomato";
-      this.ctx.fillText("Game Over", 50, 150);
-    }
-
-    drawScore() {
-      this.ctx.font = "20px Arial";
-      this.ctx.fillStyle = "#fdfdfd";
-      this.ctx.fillText(this.score, 10, 25);
+    gameOver() {
+      this.isGameOver = true;
     }
   }
+
   const canvas = document.querySelector("canvas");
   if (typeof canvas.getContext === "undefined") {
     return;
   }
 
-  new Game(canvas);
+  const ballNum = Math.floor(rand(2, 6));
+  const game = new Game(canvas);
+  const paddle = new Paddle(canvas);
+  const balls = new Balls(ballNum, canvas, paddle, game);
+  new Screen(canvas, paddle, balls, game);
 })();
