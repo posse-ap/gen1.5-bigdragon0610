45555ac96b7b5c5cce740ce859f0bdf86b003ea7
ryudai <bigdragon.06102001@gmail.com>
2022-10-03 04:39:00 +0900

    不要なクラスを削除

--- client/src/components/molecules/AfterRecordModalContent.tsx
+++ client/src/components/molecules/AfterRecordModalContent.tsx
@@ -7,7 +7,7 @@ const AfterRecordModalContent: VFC = () => {
     <section>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         <p className='text-[#bde360] text-center text-lg'>AWESOME!</p>
-        <p className='mx-auto mt-2 peer-checked:bg-[#0f71bd] w-20 h-20 flex justify-center items-center bg-[#bde360] rounded-full'>
+        <p className='mx-auto mt-2 w-20 h-20 flex justify-center items-center bg-[#bde360] rounded-full'>
           <FontAwesomeIcon icon={faCheck} className='text-white w-10 h-10' />
         </p>
         <p className='text-center mt-8 text-xl'>
